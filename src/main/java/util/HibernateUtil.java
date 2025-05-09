827b6ac82ea95079de9bf98bee412aaa47f79d6e
Mohamed Shukry <123892337+mhdshukry@users.noreply.github.com>
2025-05-09 15:51:12 +0530

    Create HibernateUtil.java

---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    @Getter
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed." + ex);
+        }
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
